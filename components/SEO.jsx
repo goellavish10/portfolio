f363aec873138d9d40a1f3e716feadd320294ba1
Lavish Goyal <goellavish10@gmail.com>
2021-11-07 16:16:26 +0530

    Google Analytics

--- components/SEO.jsx
+++ components/SEO.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+// import { NextScript } from 'next/document';
 import PropTypes from "prop-types";
 
 function SEO({ data }) {
@@ -7,7 +8,7 @@ function SEO({ data }) {
 		<Head>
 			<title>{data.title}</title>
 			<meta name="title" content={data.title} />
-			<meta name="author" content="Hanzla Tauqeer" />
+			<meta name="author" content="Lavish Goyal" />
 			<meta name="description" content={data.description} />
 			<meta name="keywords" content={data.keywords.join(", ")} />
 			<link rel="canonical" href={data.url} />
@@ -44,6 +45,18 @@ function SEO({ data }) {
 				async
 				src="https://code.iconify.design/1/1.0.4/iconify.min.js"
 			></script>
+			{/* Global site tag (gtag.js) - Google Analytics  */}
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-M2EEKHM2PJ" />
+			<script dangerouslySetInnerHTML={{
+				__html: `window.dataLayer = window.dataLayer || [];
+				function gtag(){
+					dataLayer.push(arguments);
+				}
+				gtag('js', new Date());
+
+				gtag('config', 'G-M2EEKHM2PJ');`}} />
+
+
 		</Head>
 	);
 }
