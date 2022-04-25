080bbcc02429913c8382889945c703785a5306fe
goellavish10 <goellavish10@gmail.com>
2022-04-26 00:36:05 +0530

    new company

--- portfolio.js
+++ portfolio.js
@@ -1,197 +1,205 @@
 import emoji from "react-easy-emoji";
 
 export const greetings = {
-	name: "Lavish Goyal",
-	title: "Hi all, I'm Lavish",
-	description:
-		"A passionate Full Stack Web Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks.",
-	resumeLink: "https://drive.google.com/file/d/1bIoY1KCOCK30LDGc4Df_NWelvV4zezpp/view?usp=sharing",
+  name: "Lavish Goyal",
+  title: "Hi all, I'm Lavish",
+  description:
+    "A passionate Full Stack Web Developer having an experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks.",
+  resumeLink:
+    "https://drive.google.com/file/d/1bIoY1KCOCK30LDGc4Df_NWelvV4zezpp/view?usp=sharing",
 };
 
 export const openSource = {
-	githubUserName: "goellavish10",
+  githubUserName: "goellavish10",
 };
 
 export const contact = {};
 
 export const socialLinks = {
-	instagram: "https://www.instagram.com/lavi_g10/",
-	twitter: "https://twitter.com/lavishG10",
-	github: "https://github.com/goellavish10",
-	linkedin: "https://www.linkedin.com/in/goellavish10",
+  instagram: "https://www.instagram.com/lavi_g10/",
+  twitter: "https://twitter.com/lavishG10",
+  github: "https://github.com/goellavish10",
+  linkedin: "https://www.linkedin.com/in/goellavish10",
 };
 
 export const skillsSection = {
-	title: "What I do",
-	subTitle:
-		"A MERN STACK DEVELOPER AND PASSIONATE ABOUT BACKEND",
-	skills: [
-		emoji(
-			"⚡ Developing interactive front-end applications with frameworks like tailwind, bootstrap etc"
-		),
-		emoji(
-			"⚡ Integration of third party services such as Heroku / Digital Ocean"
-		),
-	],
+  title: "What I do",
+  subTitle: "A MERN STACK DEVELOPER AND PASSIONATE ABOUT BACKEND",
+  skills: [
+    emoji(
+      "⚡ Developing interactive front-end applications with frameworks like tailwind, bootstrap etc"
+    ),
+    emoji(
+      "⚡ Integration of third party services such as Heroku / Digital Ocean"
+    ),
+  ],
 
-	softwareSkills: [
-		{
-			skillName: "html-5",
-			fontAwesomeClassname: "vscode-icons:file-type-html",
-		},
-		{
-			skillName: "css3",
-			fontAwesomeClassname: "vscode-icons:file-type-css",
-		},
-		{
-			skillName: "sass",
-			fontAwesomeClassname: "logos:sass",
-		},
-		{
-			skillName: "JavaScript",
-			fontAwesomeClassname: "logos:javascript",
-		},
-		{
-			skillName: "TypeScript",
-			fontAwesomeClassname: "logos:typescript-icon",
-		},
-		{
-			skillName: "reactjs",
-			fontAwesomeClassname: "vscode-icons:file-type-reactjs",
-		},
-		{
-			skillName: "nextjs",
-			fontAwesomeClassname: "vscode-icons:file-type-next",
-		},
-		{
-			skillName: "nodejs",
-			fontAwesomeClassname: "logos:nodejs-icon",
-		},
-		{
-			skillName: "npm",
-			fontAwesomeClassname: "vscode-icons:file-type-npm",
-		},
-		{
-			skillName: "mongoDB",
-			fontAwesomeClassname: "vscode-icons:file-type-mongo",
-		},
-		// {
-		// 	skillName: "firebase",
-		// 	fontAwesomeClassname: "logos:firebase",
-		// },
-		{
-			skillName: "git",
-			fontAwesomeClassname: "logos:git-icon",
-		},
-		{
-			skillName: "cpp",
-			fontAwesomeClassname: "vscode-icons:file-type-cpp",
-		},
-	],
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "vscode-icons:file-type-html",
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "vscode-icons:file-type-css",
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "logos:sass",
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "logos:javascript",
+    },
+    {
+      skillName: "TypeScript",
+      fontAwesomeClassname: "logos:typescript-icon",
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+    },
+    {
+      skillName: "nextjs",
+      fontAwesomeClassname: "vscode-icons:file-type-next",
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "logos:nodejs-icon",
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "vscode-icons:file-type-npm",
+    },
+    {
+      skillName: "mongoDB",
+      fontAwesomeClassname: "vscode-icons:file-type-mongo",
+    },
+    // {
+    // 	skillName: "firebase",
+    // 	fontAwesomeClassname: "logos:firebase",
+    // },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "logos:git-icon",
+    },
+    {
+      skillName: "cpp",
+      fontAwesomeClassname: "vscode-icons:file-type-cpp",
+    },
+  ],
 };
 
 export const SkillBars = [
-	{
-		Stack: "Frontend/Design", //Insert stack or technology you have experience in
-		progressPercentage: "60", //Insert relative proficiency in percentage
-	},
-	{
-		Stack: "Backend",
-		progressPercentage: "80",
-	},
-	{
-		Stack: "Programming",
-		progressPercentage: "70",
-	},
+  {
+    Stack: "Frontend/Design", //Insert stack or technology you have experience in
+    progressPercentage: "60", //Insert relative proficiency in percentage
+  },
+  {
+    Stack: "Backend",
+    progressPercentage: "80",
+  },
+  {
+    Stack: "Programming",
+    progressPercentage: "70",
+  },
 ];
 
 export const educationInfo = [
-	{
-		schoolName: "Dr. B.R. Ambedkar National Institute of Technology, Jalandhar",
-		subHeader: "Bachelor Electronics & Communication Engineering",
-		duration: "December 2021 - Present",
-		desc: "Participated in various speaking contests within college.",
-		descBullets: [
-			"Core member of GDSC & Codechef Chapter NIT-J",
-			"Class representative",
-		],
-	},
-	// {
-	// 	schoolName: "Harvard",
-	// 	subHeader: "Master of Science in Computer Science",
-	// 	duration: "September 2017 - April 2019",
-	// 	desc: "Participated in the research of XXX and published 3 papers.",
-	// 	descBullets: [
-	// 		"Lorem ipsum dolor sit amet, consectetfgur adipiscing elit",
-	// 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-	// 	],
-	// },
-	// {
-	// 	schoolName: "Stanford University",
-	// 	subHeader: "Bachelor of Science in Computer Science",
-	// 	duration: "September 2013 - April 2017",
-	// 	desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-	// 	descBullets: [
-	// 		"Lorem ipsum dolorfdg sit amet, consectetur adipiscing elit",
-	// 	],
-	// },
+  {
+    schoolName: "Dr. B.R. Ambedkar National Institute of Technology, Jalandhar",
+    subHeader: "Bachelor Electronics & Communication Engineering",
+    duration: "December 2020 - Present",
+    desc: "Participated in various speaking contests within college.",
+    descBullets: [
+      "Core member of GDSC & Codechef Chapter NIT-J",
+      "Class representative",
+    ],
+  },
+  // {
+  // 	schoolName: "Harvard",
+  // 	subHeader: "Master of Science in Computer Science",
+  // 	duration: "September 2017 - April 2019",
+  // 	desc: "Participated in the research of XXX and published 3 papers.",
+  // 	descBullets: [
+  // 		"Lorem ipsum dolor sit amet, consectetfgur adipiscing elit",
+  // 		"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+  // 	],
+  // },
+  // {
+  // 	schoolName: "Stanford University",
+  // 	subHeader: "Bachelor of Science in Computer Science",
+  // 	duration: "September 2013 - April 2017",
+  // 	desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
+  // 	descBullets: [
+  // 		"Lorem ipsum dolorfdg sit amet, consectetur adipiscing elit",
+  // 	],
+  // },
 ];
 
 export const experience = [
-	{
-		role: "Full Stack Web Developer",
-		company: "Yottabite",
-		companylogo: "/img/icons/common/logo1.svg",
-		date: "October 2021 – Present",
-		desc: "Responsible for developing all the customer and seller facing tools as well as adming dashboard.",
-		// descBullets: [
-		// 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
-		// 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-		// ],
-	},
-	// {
-	// 	role: "Front-End Developer",
-	// 	company: "Github",
-	// 	companylogo: "/img/icons/common/github.svg",
-	// 	date: "May 2017 – May 2018",
-	// 	desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-	// },
-	// {
-	// 	role: "Software Engineer Intern",
-	// 	company: "Airbnb",
-	// 	companylogo: "/img/icons/common/airbnbLogo.png",
-	// 	date: "Jan 2015 – Sep 2015",
-	// 	desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-	// },
+  {
+    role: "ReactJS Developer",
+    company: "Unomonk",
+    companylogo: "/img/icons/common/logo.jpg",
+    date: "March 2022 - Present",
+    desc: "Developing the admin panel in Material UI and ReactJS",
+    // descBullets: [
+    // 	"Lorem ipsum dolor sit amet, consdfgectetur adipiscing elit",
+    // 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    // ],
+  },
+  //   {
+  //     role: "Full Stack Web Developer",
+  //     company: "Yottabite",
+  //     companylogo: "/img/icons/common/logo1.svg",
+  //     date: "October 2021 - Feburary 2022",
+  //     desc: "Responsible for developing all the customer and seller facing tools as well as adming dashboard.",
+  //   },
+
+  // {
+  // 	role: "Front-End Developer",
+  // 	company: "Github",
+  // 	companylogo: "/img/icons/common/github.svg",
+  // 	date: "May 2017 – May 2018",
+  // 	desc: "Lorem ipsum dolor sit amet, consefdctetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  // },
+  // {
+  // 	role: "Software Engineer Intern",
+  // 	company: "Airbnb",
+  // 	companylogo: "/img/icons/common/airbnbLogo.png",
+  // 	date: "Jan 2015 – Sep 2015",
+  // 	desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  // },
 ];
 
 export const projects = [
-	{
-		name: "E-Commerce Website",
-		desc: "A full stack ecommerce website (MERN STACK)",
-		github: "https://github.com/goellavish10/ecommerce",
-	},
-	{
-		name: "Link Shortner",
-		desc: "A simple Nodejs / EJS Link shortening service",
-		github: "https://github.com/goellavish10/shortner",
-	},
-	{
-		name: "Blog Wesbite",
-		desc: "Nodejs / Handlebars / MongoDB / Heroku Blog Website",
-		link: "https://eceblog.herokuapp.com/",
-	},
+  {
+    name: "E-Commerce Website",
+    desc: "A full stack ecommerce website (MERN STACK)",
+    github: "https://github.com/goellavish10/ecommerce",
+  },
+  {
+    name: "Link Shortner",
+    desc: "A simple Nodejs / EJS Link shortening service",
+    github: "https://github.com/goellavish10/shortner",
+  },
+  {
+    name: "Blog Wesbite",
+    desc: "Nodejs / Handlebars / MongoDB / Heroku Blog Website",
+    link: "https://eceblog.herokuapp.com/",
+  },
 ];
 
 export const feedbacks = [
-	{
-		name: "Hassan Mehmood",
-		feedback:
-			"We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Hanzla tauqeer has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose hanzla web developer services for u.",
-	},
-	{
-		name: "Maheen Altaf",
-		feedback:
-			"the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
-	},
+  {
+    name: "Hassan Mehmood",
+    feedback:
+      "We have been very happy with our new website! It looks professional and very easy to navigate. Our experience with the customer service at Hanzla tauqeer has been great. They handle things very efficiently and are available for any questions we have. They also keep us updated on daily reports so we know how our site is doing. I would recommend that u have choose hanzla web developer services for u.",
+  },
+  {
+    name: "Maheen Altaf",
+    feedback:
+      "the website is very nice, everything was perfectly made as i need. it just loads in moments, without giving u the chance to wait. the quality is also very amazing. i am using it without any problem. great job",
+  },
 ];
